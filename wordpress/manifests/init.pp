5e21076f8437ae95f6d228d744227a64627b1d7c
root <root@puppetworkstation.europe-west1-b.c.noble-freehold-230911.internal>
2019-05-07 11:14:39 +0000

    replaced exec by file and user commands

--- wordpress/manifests/init.pp
+++ wordpress/manifests/init.pp
@@ -1,37 +1,46 @@
 class wordpress {
 
 exec {'apt update': 
-   path => ['/usr/bin', '/usr/sbin',],	
+   path => '/usr/bin'
 }
+
 $packagenames = ['apache2', 'mysql-server', 'mysql-client', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql', 'unzip']
 $packagenames.each |String $package| {
 package {"${package}":
  ensure => latest,
  }
 }
-exec {'mysqladmin -u root password rootpassword':
-   path => ['/usr/bin', '/usr/sbin',],
+
+user { 'mysqladmin':
+ ensure => present,
+ password => 'rootpassword',
 }
+
 exec {'wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands':
      cwd => '/tmp',	
-     path => ['/usr/bin', '/usr/sbin',],	
+     path => '/usr/bin',
+     creates => '/tmp/mysqlcommands',	
 }
+
 exec {'sudo mysql -uroot -prootpassword < /tmp/mysqlcommands':
-   path => ['/usr/bin', '/usr/sbin',],
+   path => '/usr/bin'
 }
 
 exec {'wget https://wordpress.org/latest.zip':
     cwd => '/tmp',
-    path => ['/usr/bin', '/usr/sbin',],
+    path => '/usr/bin',
+    creates => '/tmp/latest.zip',
 }
 
 exec {'unzip /tmp/latest.zip -d /var/www/html':
-         path => ['/usr/bin', '/usr/sbin',],
+         path => '/usr/bin',
+         creates => '/var/www/html/wordpress',
 }
 
 exec {'wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php':
     cwd => '/var/www/html/wordpress',
-    path => ['/usr/bin', '/usr/sbin',],
+    path => '/usr/bin',
+    creates => '/var/www/html/wordpress/wp-config-sample.php',
 }
 
 file {'/var/www/html/wordpress/wp-config.php':
@@ -41,22 +50,20 @@ file {'/var/www/html/wordpress/wp-config.php':
 
 file { '/var/www/html/wordpress':
   mode => '775',
-}
-
-exec {'chown -R www-data:www-data /var/www/html/wordpress':
-            path => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  recurse => true,
+  owner => 'www-data',
 }
 
 exec {"sed -i 's/database_name_here/wordpress/' /var/www/html/wordpress/wp-config.php":
-    path => ['/bin', '/sbin', '/user/bin', '/usr/sbin',],
+    path => '/bin',
 }
 
 exec {"sed -i 's/username_here/wordpressuser/' /var/www/html/wordpress/wp-config.php":
-    path => ['/bin', '/sbin', '/user/bin', '/usr/sbin',],
+    path => '/bin',
 }
 
 exec {"sed -i 's/password_here/password/' /var/www/html/wordpress/wp-config.php":
-    path => ['/bin', '/sbin', '/user/bin', '/usr/sbin',],
+    path => '/bin',
 }
 
 exec {'service apache2 restart':
